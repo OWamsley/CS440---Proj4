f090a4a4818cac68f22b3327825fe2eaa03b9e39
OWamsley <owamsley10@gmail.com>
2022-04-12 13:25:29 -0600

    added first extra credit part + clean up printing

--- Bathroom.java
+++ Bathroom.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Bathroom {
     public static void main(String args[]) {
+        System.out.println("People Arrival Options: \n\n0) 5 arrive, 10sec delay, 5 arrive, 10sec delay, 5 arrive, 10sec delay, 5 arrive");
+        System.out.println("1) 10 arrive, 10sec delay, 10 arrive \n2) 20 arrive\n3)Extra credit randomization: 5-20 people arrive every 5-10 seconds. People stop arriving at t=30");
         Scanner input = new Scanner(System.in);
-        System.out.println("Which people arrival option? Enter 0, 1, or 2");
+        System.out.println("\nWhich people arrival option? Enter 0, 1, 2, or 3");
         int option = input.nextInt();
         ArrayList<Person> people = createPeople(option);
         bathroomAlgorithm(people);
+        
     }
 
     public static ArrayList<Person> createPeople(int setting) {
@@ -135,6 +138,34 @@ public class Bathroom {
 
             }
         }
+
+        else if (setting == 3) {
+            int curTime = 0;
+            int addedPeople;
+            int timeGap;
+            int id = 1;
+            Person newPerson;
+            while (curTime <= 30) {
+                addedPeople = random.nextInt(16) + 5;
+                timeGap = random.nextInt(6) + 5;
+                for (int x = 0; x < addedPeople; x++) {
+                    int gender;
+                    int genderOdds = random.nextInt(100);
+                    if (genderOdds < 60) {
+                        gender = 1;
+                        // female
+                    } else {
+                        gender = 0;
+                        // male
+                    }
+                    int time = random.nextInt(6) + 3;
+                    newPerson = new Person(id, gender, time, curTime);
+                    people.add(newPerson);
+                    id++;
+                }
+                curTime += timeGap;
+            }
+        }
         return people;
     }
 
@@ -142,30 +173,47 @@ public class Bathroom {
         // peopleWaiting must be in order of arrival.
         int curTime = 0;
         int curInRoom = 0;
+        // curInRoom is how many people are in the bathroom
         int curGender = 0;
         int timeLeft = 0;
-        int[] departTime = {-1, -1};
+        // timeleft is how long the current first person still needs
+        int[] departTime = { -1, -1 };
+        // departTime is used to store when the current bathroom users will leave
         ArrayList<Person> done = new ArrayList<Person>();
+        // when a person enters the bathroom the are removed from the waiting list and
+        // are added to the done list
 
         while (peopleWaiting.isEmpty() == false) {
+            // If time = a person's arrival time, print that they arrive for accurate simulation
+            for(Person x: peopleWaiting){
+                if(curTime == x.arrival){
+                    System.out.println("@t= " + curTime + "; Person " + x.id + " (" + x.genderChar + ") arrives");
+                }
+            }
+
+            // If nobody is in the bathroom, first person in line goes in.
             if (curInRoom == 0) {
                 Person curPerson = peopleWaiting.get(0);
                 curInRoom++;
                 timeLeft = curPerson.time;
                 curGender = curPerson.gender;
                 departTime[0] = curTime + curPerson.time;
-                System.out.println("@t= " + curTime + " ; Person " + curPerson.id + " (" + curPerson.genderChar + ") enters the facilities for " + curPerson.time + " minutes");
+                System.out.println("@t= " + curTime + "; Person " + curPerson.id + " (" + curPerson.genderChar
+                        + ") enters the facilities for " + curPerson.time + " minutes");
                 curPerson.setDeparture(timeLeft + curTime);
                 done.add(curPerson);
                 peopleWaiting.remove(curPerson);
             }
             if (curInRoom < 2) {
-                Person personAdded;
-                for (Person person : peopleWaiting){
-                    if((curGender == person.gender) && (person.time <= timeLeft) && (person.arrival <= curTime)){
+                // If just one person in the bathroom, another can join if they will take equal
+                // or less time than the first person, are the same gender, and have already
+                // arrived
+                for (Person person : peopleWaiting) {
+                    if ((curGender == person.gender) && (person.time <= timeLeft) && (person.arrival <= curTime)) {
                         curInRoom++;
                         departTime[1] = curTime + person.time;
-                        System.out.println("@t= " + curTime + " ; Person " + person.id + " (" + person.genderChar + ") enters the facilities for " + person.time + " minutes");
+                        System.out.println("@t= " + curTime + "; Person " + person.id + " (" + person.genderChar
+                                + ") enters the facilities for " + person.time + " minutes");
                         done.add(person);
                         person.setDeparture(curTime + person.time);
                         peopleWaiting.remove(person);
@@ -173,23 +221,21 @@ public class Bathroom {
                     }
                 }
             }
-            if(departTime[1] == curTime){
+            if (departTime[1] == curTime) {
                 // if the second person is done, they leave, leaving the first person
                 curInRoom = 1;
             }
-            if(departTime[0] == curTime){
-                //if first person in bathroom is done, then there is nobody in the bathroom.
+            if (departTime[0] == curTime) {
+                // if first person in bathroom is done, then there is nobody in the bathroom.
                 curInRoom = 0;
             }
-
             timeLeft--;
             curTime++;
-            
-
         }
         int departNo = 1;
-        for(Person i : done){
-            System.out.println("@t= " + i.departure + "; Person " + i.id + " (" + i.genderChar + ") exits (departure # " + departNo);
+        for (Person i : done) {
+            System.out.println("@t= " + i.departure + "; Person " + i.id + " (" + i.genderChar + ") exits (departure # "
+                    + departNo + ")");
             departNo++;
         }
     }
